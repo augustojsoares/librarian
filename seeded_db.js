7dae4b8929e8bdebed4cc14ce8c81c46f9733e14
Augusto Soares <augusto@Augustos-MBP.Home>
2020-12-29 18:10:15 +0000

    Added tags to seeded database

--- seeded_db.js
+++ seeded_db.js
@@ -5,7 +5,53 @@ const faker = require('faker')
  * @module data/generator
  */
 module.exports = () => {
+	/**
+	 * Generate a random integer number between 0 and an upper bound
+	 * @param {number} max The max bound for the generated number.
+	 * @returns {number} The generated random integer.
+	 */
+	const getRandomInt = max => {
+		return Math.floor(Math.random() * Math.floor(max))
+	}
+
 	const data = { books: [] }
+	/**
+	 * Generate a list of tags.
+	 * @param {number} [maxTagCount=3] The maximum number of tags per book. Defaults to three.
+	 * @param {number} [availableTags] The tag list from which to pick. Defaults to the presented list.
+	 * @returns {string[]} The sum of the two numbers.
+	 */
+	const getTags = (
+		maxTagCount = 3,
+		availableTags = [
+			'FICTION',
+			'MISTERY',
+			'DRAMA',
+			'SCI-FI',
+			'FANTASY',
+			'ADVENTURE',
+			'TECHNICAL',
+			'ROMANCE',
+			'HORROR',
+			'SHORT',
+			'COOKING',
+		]
+	) => {
+		const maxTagIndex = availableTags.length - 1
+
+		const tagCount = getRandomInt(maxTagCount)
+		const tags = new Set() // using a Set facilitates preventing repeat values
+
+		// if tagCount is zero and therefore falsy, shortcircuit the conditional and skip
+		while (tagCount && tags.size <= tagCount) {
+			const nextTagIndex = getRandomInt(maxTagIndex)
+			const nextTag = availableTags[nextTagIndex]
+
+			tags.add(nextTag) // if tags already contains nextTag, the Set size remains the same and execution continues
+		}
+
+		return Array.from(tags) // convert the set to an array before returning
+	}
 
 	// Create 1000 books
 	for (let i = 0; i < 1000; i++) {
@@ -14,8 +60,9 @@ module.exports = () => {
 			title: faker.company.catchPhrase(),
 			description: faker.lorem.paragraph(),
 			author: faker.name.findName(),
-			publicationDate: faker.date.between(new Date('1870-01-01'),new Date()),
+			publicationDate: faker.date.between(new Date('1870-01-01'), new Date()),
 			coverImage: 'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
+			tags: getTags(),
 		})
 	}
 	return data
